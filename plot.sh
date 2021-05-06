e5bb7ca113ed6bd09c5d170f867254a038800af5
ImagineBreaker <breakertt@outlook.com>
2021-05-07 01:27:34 +0800

    Update plot.sh

--- plot.sh
+++ plot.sh
@@ -14,4 +14,4 @@ sleep $((SLEEP))
 
 # Plotting
 PATH=$PATH:/usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon
-numactl --cpunodebind=$NODE --localalloc chia plots create -k 32 -n 99 -b 8000 -t $TMP -d $DST 2>&1 | tee $LOG
+chia plots create -k 32 -n 99 -b 8000 -t $TMP -d $DST 2>&1 | tee $LOG
