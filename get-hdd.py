f340bc46a808bfb120b7fdd23dca0b112745eaa6
ImagineBreaker <breakertt@outlook.com>
2021-05-29 13:30:19 +0800

    Update get-hdd.py

--- get-hdd.py
+++ get-hdd.py
@@ -7,7 +7,7 @@ with open("hdd_list.txt") as f:
 hdd_list_available = []
 for i in range(len(hdd_list)):
   total, used, free = shutil.disk_usage("/mnt/" + hdd_list[i])
-  if free > 107000000*4:
+  if free > 101*1024*1024*1024*4:
     hdd_list_available.append("/mnt/" + hdd_list[i])
     
 hdd_selected = random.choice(hdd_list_available)
